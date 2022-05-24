e82a3635ce2fa23761d3dcc79f57accc1a1e098f
Hau Phan T. VN.Danang <hau.phan@monstar-lab.com>
2022-05-24 15:58:07 +0700

    chapter_10

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,11 +3,12 @@ class UsersController < ApplicationController
   before_action :correct_user,   only: [:edit, :update]
 
   def index
-    @users = User.all.page params[:page]
+    @users = User.where(activated: true).page params[:page]
   end
 
   def show
     @user = User.find(params[:id])
+    redirect_to root_url, status: 303 and return unless @user.activated?
   end
 
   def new
@@ -17,10 +18,11 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)    # Not the final implementation!
     if @user.save
-      flash[:success] = "Welcome to the Sample App!"
-      redirect_to @user
+      @user.send_activation_email
+      flash[:info] = "Please check your email to activate your account."
+      redirect_to root_url, status: 303
     else
-      render :new
+      render :new, status: 303
       flash.now[:danger] = "Signup fails!"
     end
   end
